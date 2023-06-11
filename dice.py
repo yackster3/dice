e0b5272180183cd2a8ea16084dd4a40efc10abc9
Stephen <savyacktman@yahoo.com>
2023-06-10 21:55:13 -0600

    making the d4 and identifying problems, haven't updated readme

--- dice.py
+++ dice.py
@@ -2,6 +2,23 @@ from ursina import *
 import random as rand
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+"""
+ TODO:
+    d6:
+        need to color the individual faces, get numbers on each one and in the
+        proper positions (only 6 possible starting positions required)
+
+        the settling animation requires some work, only 2 angles should be used
+        but determining which 2 will be interesting.
+
+    d4:
+        Die needs new focus points on where exactly it's supposed to fall.
+        the faces aren't all showing at all times.
+
+        it still falls like a d6
+
+    a lot more debugging
+"""
 
 class d6(Entity):
     def __init__(self, **kwargs):
@@ -55,7 +72,7 @@ class d6(Entity):
             #Impact on the floor
 
             impact = abs(self.speed_y - (self.speed_y * self.rebound))
-            
+
             #reaction based on the impact of the floor
             #Here it is becoming static
             if impact < .35:
@@ -86,9 +103,9 @@ class d6(Entity):
                 self.speed_y = -self.rebound * self.speed_y
 
                 #rotation bounce
-                self.omega_x = 3 * self.omega_x * (.5 - rand.random())
-                self.omega_y = 3 * self.omega_y * (.5 - rand.random())
-                self.omega_z = 3 * self.omega_z * (.5 - rand.random())
+                self.omega_x = 3.5 * self.omega_x * (.5 - rand.random())
+                self.omega_y = 3.5 * self.omega_y * (.5 - rand.random())
+                self.omega_z = 3.5 * self.omega_z * (.5 - rand.random())
 
 
 
@@ -113,13 +130,134 @@ class d6(Entity):
         #toggle pause function
         if key == "p":
             self.pause = not self.pause
+class d4(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+
+        self.verts = ((1,0,0),(-1/2,0,sqrt(3)/2),(-1/2,0,-sqrt(3)/2),(0,sqrt(3+sqrt(3)),0))
+        self.tris = (0,1,2,0,2,3,0,1,3,1,2,3)
+        self.colors = (color.random_color(), color.random_color(), color.random_color(), color.random_color())
+
+        # TODO:  What are these?
+        self.uvs = ((1,0), (0,1), (0,0), (1,1))
+        self.norms = ((0,0,-1),)*len(self.verts)
+
+        self.model = Mesh(vertices = self.verts, triangles = self.tris, uvs = self.uvs,
+                            normals = self.norms, colors = self.colors)
+
+        self.texture = "white_cube"
+        self.collider = 'box'
+        #self.rgb = (0,255,0)
+        self.rolling = False
+        self.rolling_speed = 20
+        self.omega_x = 0
+        self.omega_y = 0
+        self.omega_z = 0
+        self.speed_x = 0
+        self.speed_y = 0
+        self.speed_z = 0
+        self.rebound = .75
+        self.pause = False
+        self.floor = False
+
+
+    def roll(self):
+        self.omega_x = rand.random()*1000
+        self.omega_y = rand.random()*1000
+        self.omega_z = rand.random()*1000
+        self.speed_y = self.rolling_speed
+        self.floor = False
+
+
+    def motion(self):
+        #rotation speed
+        self.rotation_x += self.omega_x * time.dt
+        self.rotation_y += self.omega_y * time.dt
+        self.rotation_z += self.omega_z * time.dt
+
+        #translation speed
+        self.x += self.speed_x * time.dt
+        self.y += self.speed_y * time.dt
+        self.z += self.speed_z * time.dt
+
+    def gravity(self):
+        if not self.floor:
+            #gravity
+            self.speed_y -= 9.81 * time.dt
+
+
+    def bounce(self):
+        #floor
+        if self.y < -3:
+            self.y = -3
+            #Impact on the floor
+
+            impact = abs(self.speed_y - (self.speed_y * self.rebound))
+
+            #reaction based on the impact of the floor
+            #Here it is becoming static
+            if impact < .35:
+                self.omega_x = 0
+                self.omega_y = 0
+                self.omega_z = 0
+                self.y = -3
+                self.floor = True
+
+            #Here it is nestling
+            elif impact < .9:
+                self.speed_y = -self.rebound * self.speed_y
+
+                X = self.rotation_x + 45
+                Y = self.rotation_y + 45
+                Z = self.rotation_z + 45
+
+                X = (X // 90) * 90
+                Y = (Y // 90) * 90
+                Z = (Z // 90) * 90
+
+                self.omega_x = X - self.rotation_x
+                self.omega_y = Y - self.rotation_y
+                self.omega_z = Z - self.rotation_z
+
+            #Here it is bouncing wildly
+            else:
+                self.speed_y = -self.rebound * self.speed_y
+
+                #rotation bounce
+                self.omega_x = 3.5 * self.omega_x * (.5 - rand.random())
+                self.omega_y = 3.5 * self.omega_y * (.5 - rand.random())
+                self.omega_z = 3.5 * self.omega_z * (.5 - rand.random())
+
+
+
+    def update(self):
+        #keep rotation angle convienient
+        self.rotation_x = self.rotation_x % 360
+        self.rotation_y = self.rotation_y % 360
+        self.rotation_z = self.rotation_z % 360
+
+        #Check if it's in play
+        if not self.pause:
+            self.motion()
+            self.gravity()
+            self.bounce()
+
+
+    def input(self, key):
+        #roll the die
+        if key == "space":
+            self.roll()
+            print(self.colors)
+        #toggle pause function
+        if key == "p":
+            self.pause = not self.pause
 
 app = Ursina()
 
-d = d6()
+d = d4()
 floor = Entity(model = "plane", position = (0,-4,0), scale = (100,.1,100), color = color.rgb(25,165,25), texture= 'grass', collider = 'mesh')
 Sky()
-
+print(color.random_color)
 camera.position = (0,5,-55)
 
 def update():
